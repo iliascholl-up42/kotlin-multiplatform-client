cd8aef45a3b408b8150e4fd29dcec50dded4074f
Ilia Scholl <ilia.scholl@up42.com>
2021-12-01 09:27:05 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    kotlin("multiplatform") version "1.6.0"
+}
+
+group = "com.up42.makeathon"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(IR) {
+        binaries.executable()
+        browser {
+
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+    }
+}
